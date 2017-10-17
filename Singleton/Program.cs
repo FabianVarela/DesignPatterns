af58e094c2f16a64ad41e83dede30ea72319cfd4
Fabian Varela Bonett <fabian-303@hotmail.com>
2017-10-16 22:00:41 -0500

    Create Project Singleton Pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using Singleton.Singleton;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonSingleton person = PersonSingleton.GetInstance();
+
+            person.Id = "1234567890";
+            person.Name = "Fabian Varela Bonett";
+            person.Email = "fabian-303@hotmail.com";
+
+            ShowData();
+        }
+
+        public static void ShowData()
+        {
+            PersonSingleton person = PersonSingleton.GetInstance();
+            person.ShowData();
+        }
+    }
+}
---
+++ Singleton/Singleton/PersonSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Singleton.Singleton
+{
+    public class PersonSingleton
+    {
+        private static PersonSingleton _instance = null;
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        private PersonSingleton() { }
+
+        public static PersonSingleton GetInstance()
+        {
+            if (_instance == null)
+                _instance = new PersonSingleton();
+
+            return _instance;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Email: {Email}");
+
+            Console.ReadKey();
+        }
+    }
+}
